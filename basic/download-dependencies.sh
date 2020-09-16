e3dafcac3931412ee82e582163cc4c6eeca283c1
filipecsimoes <835586+filipesimoes@users.noreply.github.com>
2020-09-16 17:23:25 -0300

    #1

---
+++ basic/download-dependencies.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+if [[ ${DOCKER_TAG} == 'latest' ]]; then
+	depVersion=$(curl -s https://repo1.maven.org/maven2/com/github/euler-io/euler-elasticsearch/maven-metadata.xml | grep '<latest>' | sed "s/.*<latest>\([^<]*\)<\/latest>.*/\1/")
+else
+	depVersion=$DOCKER_TAG
+fi
+echo "Using com.github.euler-io:euler-elasticsearch:$depVersion"
+
+mkdir /dependencies
+mvn dependency:copy-dependencies -Deuler.version=${depVersion} -DoutputDirectory=/dependencies -f ./pom.xml
