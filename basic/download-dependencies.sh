f37023d5a6b2741184bcf64268a1f647841c5ea9
Filipe Simoes <835586+filipesimoes@users.noreply.github.com>
2020-09-21 11:00:17 -0300

    #2

--- basic/download-dependencies.sh
+++ basic/download-dependencies.sh
@@ -9,4 +9,4 @@ fi
 echo "Using com.github.euler-io:euler-elasticsearch:$depVersion"
 
 mkdir /dependencies
-mvn dependency:copy-dependencies -Deuler.version=${depVersion} -DoutputDirectory=/dependencies -f ./pom.xml
+mvn dependency:copy-dependencies -Deuler.version=${depVersion} -DoutputDirectory=/dependencies -f $(dirname "$(readlink -f "$0")")/pom.xml
