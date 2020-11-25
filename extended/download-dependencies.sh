1277d5818f5000c47b7753c633ba115fa2208e32
Filipe Simoes <835586+filipesimoes@users.noreply.github.com>
2020-11-24 22:30:41 -0300

    #1

--- extended/download-dependencies.sh
+++ extended/download-dependencies.sh
@@ -9,4 +9,4 @@ fi
 echo "Using: $depVersion"
 
 mkdir /dependencies
-mvn dependency:copy-dependencies -Deuler.version=${depVersion} -DoutputDirectory=/dependencies -f $(dirname "$(readlink -f "$0")")/pom.xml
+mvn dependency:copy-dependencies -Deuler.version=${depVersion} -DincludeScope=runtime -DoutputDirectory=/dependencies -f $(dirname "$(readlink -f "$0")")/pom.xml
